989fb19c43989f5a919943ec453f1dc2c26f6be2
heojihun <karais89@gmail.com>
2016-02-21 10:34:20 +0900

    p 100

---
+++ UnityProject/Assets/Chapter2/Scripts/DelegateUsage.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateUsage : MonoBehaviour 
+{
+	public delegate void EventHandler(int Paraml, int Param2);
+
+	public EventHandler[] EH = new EventHandler[10];
+
+	void Awake() {
+		EH[0] = HandleMyEvent;
+	}
+
+	void Start() {
+		foreach(EventHandler e in EH) {
+			if(e != null) {
+				e(0, 0);
+			}
+		}
+	}
+
+	void HandleMyEvent(int Param1, int Param2) {
+		Debug.Log("Event Called");
+	}
+}
