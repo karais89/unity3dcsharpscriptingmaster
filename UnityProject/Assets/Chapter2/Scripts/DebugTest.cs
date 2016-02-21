0604ebe14d2ec6806c1297a9bf02b7998f7ff633
heojihun <karais89@gmail.com>
2016-02-21 11:45:23 +0900

    chapter2 complte

---
+++ UnityProject/Assets/Chapter2/Scripts/DebugTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Transform[] Objs = Object.FindObjectsOfType<Transform>();
+
+		for(int i = 0; i < Objs.Length; i++) {
+			Objs[i].position = Vector3.zero;
+		}
+
+		Func01();
+	}
+
+	void Func01() {
+		Func02();
+	}
+
+	void Func02() {
+		Func03();
+	}
+
+	void Func03() {
+		Debug.Log("Entered Function 3");
+	}
+}
