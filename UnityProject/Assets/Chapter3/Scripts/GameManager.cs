311f36de199a9bf263c94fc2bd8f4328258d9f71
heojihun <karais89@gmail.com>
2016-02-21 12:27:47 +0900

    chapter3 complete

---
+++ UnityProject/Assets/Chapter3/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance {
+		get {
+			return instance;
+		}
+	}
+
+	private static GameManager instance = null;
+
+	public int HighScore = 0;
+
+	public bool IsPaused = false;
+	public bool InputAllowed = true;
+
+	void Awake() {
+		if(instance) {
+			DestroyImmediate(gameObject);
+			return;
+		}
+
+		instance = this;
+		
+		DontDestroyOnLoad(gameObject);
+	
+		Debug.Log("Awake");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UnityProject/Assets/Chapter3/Scripts/MyCustomComponent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCustomComponent : MonoBehaviour {
+	private Transform ThisTransform = null;
+	// Use this for initialization
+	void Start () {
+		ThisTransform = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(ThisTransform != null) {
+			ThisTransform.localPosition +=
+				Time.deltaTime * 10.0f * ThisTransform.forward;
+		}
+	}
+}
